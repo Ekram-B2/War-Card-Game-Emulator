3faef94de3d602e9303167623931b813c8ff3bfd
Ekram Bhuiyan <ekrambhuiyan2@gmail.com>
2020-06-23 13:28:32 -0400

    Added changes to database client to make it more readable

--- war-backend/databaseclient/client.py
+++ war-backend/databaseclient/client.py
@@ -15,19 +15,27 @@ class SqlLiteClient(Client):
         self.connection_string = connection_string
    
     def test_connection(self):
+        """
+        :rtype is_success : boolean
+        """
         # 1. Try establishing a connection
         conn = None
+        is_success = False
         try:
             conn = sqlite3.connect(self.connection_string)
+            is_success = True
         except Error as e:
-
             print("SYSTEM-ERROR: unable to connect to database: {}".format(e))
+            is_success = False
         finally:
             # 2. Regardless of success or failure, close the connection if its still active
             if conn:
                 conn.close()
+            return is_success
 
-
+    def __str__(self):
+        return "SQLite Client"
+        
 def get_database_client(clientType, connection_string):
     # 1. return a run time client based on the type
     if clientType == "sqlite":
